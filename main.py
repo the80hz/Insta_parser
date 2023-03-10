4f5536a29650ed90302ecf7b08b7db8414062621
the80hz <dnl.vlgv@mail.ru>
2023-03-10 15:36:40 +0400

    испрваил исключение и оптимизировал удаление первой строки из файла с постами

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from instagrapi import Client
 from datetime import datetime
+
 import auth_data
 
 
@@ -8,31 +9,39 @@ def main():
     client.login(auth_data.username[1], auth_data.password[1])
 
     while True:
-        # read first line in file
-        post_url = open('posts.csv', 'r').readline()
+        user = ''
+        # read lines in file
+        with open('posts.csv', 'r') as f:
+            lines = f.readlines()
+
+        # get the post url
+        post_url = lines[0].strip()
+
         # get the pk
         pk = client.media_pk_from_url(post_url)
+
         # get the user
-        # if MediaNotFound error, then delete the first line in the file
         try:
-            user = client.media_info(pk).user  # type: ignore
-        except:
-            with open('posts.csv', 'r') as f:
-                lines = f.readlines()
-            with open('posts.csv', 'w') as f:
-                for line in lines[1:]:
-                    f.write(line)
-            continue
+            user = client.media_info(pk).user
+        except Exception as e:
+            if 'Media not found or unavailable' in str(e):
+                print('Media not found or unavailable')
+                with open('posts.csv', 'w') as f:
+                    for line in lines[1:]:
+                        f.write(line)
+                continue
+            else:
+                raise e
 
         # write the str(user) to a file
         with open('users.txt', 'a') as w:
             w.write(str(user) + '\n')
+
         # delete the first line in the file
-        with open('posts.csv', 'r') as f:
-            lines = f.readlines()
         with open('posts.csv', 'w') as f:
             for line in lines[1:]:
                 f.write(line)
+
         # print the time
         end = datetime.now().timestamp()
         print(f'{round(end - start, 2)} sec')
