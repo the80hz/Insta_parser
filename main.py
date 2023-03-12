81573b28d326c216c0eee54f1e2c0d1cd32bebc4
the80hz <dnl.vlgv@mail.ru>
2023-03-12 11:44:24 +0400

    корректная аутентификация

--- main.py
+++ main.py
@@ -8,29 +8,26 @@ from selenium.webdriver.common.by import By
 from auth_data import username, password
 
 
-def auth(driver, _username, _password):
-    # Авторизация
-    driver.get("https://www.instagram.com/accounts/login/")
-    time.sleep(random.randrange(2, 4))
+def auth(_driver, _username, _password):
+    _driver.get("https://www.instagram.com/accounts/login/")
+    time.sleep(6)
 
-    username_input = driver.find_element(By.NAME, "username")
+    username_input = _driver.find_element(By.NAME, "username")
     username_input.clear()
     username_input.send_keys(_username)
 
-    password_input = driver.find_element(By.NAME, "password")
+    password_input = _driver.find_element(By.NAME, "password")
     password_input.clear()
     password_input.send_keys(_password)
 
     password_input.send_keys(Keys.RETURN)
-
-    time.sleep(random.randrange(2, 4))
+    time.sleep(6)
 
 
 if __name__ == "__main__":
-    # что вы хотите сделать 1 - спарсить новые посты, 2 - спарсить посты из файла
     while True:
         try:
-            choice = int(input("Что вы хотите сделать? \n1 - спарсить новые посты \n2 - спарсить посты из файла: \n"))
+            choice = int(input("1 - Parse new posts \n2 - Parse from file\n"))
             if choice == 1:
                 break
             elif choice == 2:
@@ -43,3 +40,5 @@ if __name__ == "__main__":
     # auth
     driver = webdriver.Chrome()
     auth(driver, username[0], password[0])
+
+
